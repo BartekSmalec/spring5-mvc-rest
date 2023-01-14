7779b10a7822477978dcf58022a45936df4688c1
bartek <smalecbartlomiej@gmail.com>
2023-01-14 11:36:00 +0100

    Init Spring Boot project

---
+++ src/test/java/com/barteksmalec/spring5mvcrest/Spring5MvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barteksmalec.spring5mvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5MvcRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
